1b3d5c9be28929ef329b1cc7ed7e69abd8c18eab
Yogesh Ojha <yogesh.ojha11@gmail.com>
2016-12-10 14:23:10 +0530

    sc2

--- app/src/main/java/com/yogeshojha/blooddonation/signup2.java
+++ app/src/main/java/com/yogeshojha/blooddonation/signup2.java
@@ -1,12 +1,27 @@
 package com.yogeshojha.blooddonation;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
-
+import customfonts.MyEditText;
+import customfonts.MyTextView;
 public class signup2 extends AppCompatActivity {
-
+    private Boolean flag = false;
+    public  ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +31,66 @@ public class signup2 extends AppCompatActivity {
                 R.array.blood_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        MyEditText location = (MyEditText) findViewById(R.id.inploc);
+        MyTextView getloc = (MyTextView) findViewById(R.id.getloc);
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+    }
+    public void getlocation(View v)
+    {
+        flag = displayGpsStatus();
+        if (flag) {
+            pb.setVisibility(View.VISIBLE);
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    pb.setVisibility(View.INVISIBLE);
+                }
+            }, 2500);
+        }
+        else
+        {
+            alertbox("Gps Status!!", "Your GPS is: OFF");
+        }
+    }
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+        boolean gpsStatus = Settings.Secure
+                .isLocationProviderEnabled(contentResolver,
+                        LocationManager.GPS_PROVIDER);
+        if (gpsStatus) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+    protected void alertbox(String title, String mymessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your Device's GPS is Disabled, Please turn it on to get the location automatically.")
+                .setCancelable(false)
+                .setTitle("Gps Status")
+                .setPositiveButton("Gps On",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // finish the current activity
+                                // AlertBoxAdvance.this.finish();
+                                Intent myIntent = new Intent(
+                                        Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(myIntent);
+                                dialog.cancel();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // cancel the dialog box
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
