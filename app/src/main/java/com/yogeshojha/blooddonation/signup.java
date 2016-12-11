78ff55e2826f2f709da0cdd4f76a0d16476c5c37
Yogesh Ojha <yogesh.ojha11@gmail.com>
2016-12-11 11:54:59 +0530

    Done

--- app/src/main/java/com/yogeshojha/blooddonation/signup.java
+++ app/src/main/java/com/yogeshojha/blooddonation/signup.java
@@ -1,12 +1,17 @@
 package com.yogeshojha.blooddonation;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import customfonts.MyEditText;
 
 public class signup extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +19,10 @@ public class signup extends AppCompatActivity {
     }
     public void sgn2(View v)
     {
+        MyEditText text = (MyEditText) findViewById(R.id.name);
+        String value = text.getText().toString();
         Intent iinent= new Intent(signup.this,signup2.class);
+        iinent.putExtra("Name", value);
         startActivity(iinent);
     }
 }
--- app/src/main/java/com/yogeshojha/blooddonation/signup2.java
+++ app/src/main/java/com/yogeshojha/blooddonation/signup2.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +18,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import customfonts.MyEditText;
 import customfonts.MyTextView;
+
+import static android.R.attr.defaultValue;
+
 public class signup2 extends AppCompatActivity {
     private Boolean flag = false;
     public  ProgressBar pb;
     public MyEditText locationinp;
+    String URL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +64,7 @@ public class signup2 extends AppCompatActivity {
                 @Override
                 public void run() {
                     pb.setVisibility(View.INVISIBLE);
-                    locationinp.setText("Hosur Rd, Wellington Paradise, Singasandra, Bengaluru, Karnataka 560068");
+                    locationinp.setText("HosurRd,WellingtonParadise,Singasandra,Bengaluru,Karnataka560068");
                 }
             }, 2500);
         }
@@ -97,7 +114,56 @@ public class signup2 extends AppCompatActivity {
     }
     public void sgn3(View v)
     {
+
+        String aname = getIntent().getStringExtra("Name");
+        MyEditText text = (MyEditText) findViewById(R.id.phone);
+        String phone = text.getText().toString();
+
+        MyEditText text1 = (MyEditText) findViewById(R.id.inploc);
+        String loctn = text1.getText().toString();
+
+        Spinner sp = (Spinner) findViewById(R.id.blood_spinner);
+        String spn = sp.getSelectedItem().toString();
+        aname.replaceAll("\\s+","");
+        spn.replaceAll("\\s+","");
+        URL = "http://kyampus.in/blood/add.php?name="+aname+"&phone="+phone+"&location="+loctn+"&blood="+spn;
+        System.out.println(URL);
+        new RequestTask().execute(URL);
         Intent iinent= new Intent(signup2.this,signup3.class);
         startActivity(iinent);
+    }
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+        }
     }
 }
